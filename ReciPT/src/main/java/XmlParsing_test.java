6955939448820e90d6cc1b968b00bcd114527524
JHyun0302 <wogustkfkd11@gmail.com>
2023-04-13 21:54:49 +0900

    순서&재료 파싱문제 해결

--- ReciPT/src/main/java/XmlParsing_test.java
+++ ReciPT/src/main/java/XmlParsing_test.java
@@ -20,7 +20,7 @@ public class XmlParsing_test {
     public static void main(String[] args) {
         int page = 1; // 페이지 초기값
         int startIdx = 1;
-        int endIdx = 2; // 100개씩 호출
+        int endIdx = 2; // 2개씩 호출
         try {
             while (true) {
                 // parsing할 url 지정(API 키 포함해서)
@@ -32,11 +32,11 @@ public class XmlParsing_test {
 
                 // root tag
                 doc.getDocumentElement().normalize();
-                System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+//                System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
 
                 // 파싱할 tag
                 NodeList nList = doc.getElementsByTagName("row");
-                System.out.println("파싱할 리스트 수 : " + nList.getLength());
+//                System.out.println("파싱할 리스트 수 : " + nList.getLength());
 
                 for (int temp = 0; temp < nList.getLength(); temp++) {
                     Node nNode = nList.item(temp);
@@ -44,17 +44,35 @@ public class XmlParsing_test {
                         Element eElement = (Element) nNode;
                         System.out.println("######################");
                         //System.out.println(eElement.getTextContent());
-                        System.out.println("음식이름  : " + getTagValue("RCP_NM", eElement));
-                        System.out.println("식재료  : " + getTagValue("RCP_PARTS_DTLS", eElement));
-                        System.out.println("조리방법  : " + getTagValue("RCP_WAY2", eElement));
-                        System.out.println("해시태그 : " + getTagValue("HASH_TAG", eElement));
-                        System.out.println("썸네일  : " + getTagValue("ATT_FILE_NO_MK", eElement));
+                        System.out.println(getTagValue("RCP_NM", eElement)); //레시피 이름
+
+
+                        String howToCook = getTagValue("RCP_PARTS_DTLS", eElement); //식재료
+                        String[] ingredients = howToCook.split("\\\\:|●");
+                        for (int i = 0; i < ingredients.length; i++) { //메인 메뉴 & 부 메뉴 식재료 나누기
+                            String[] subMenu = ingredients[i].toString().split(":");
+
+                            for (int j = 0; j < subMenu.length; j++) {
+                                System.out.println(subMenu[j]);
+                            }
+                        }
+
+
+                        System.out.println(getTagValue("RCP_WAY2", eElement)); //조리방법
+                        System.out.println(getTagValue("HASH_TAG", eElement)); //해시태그
+                        System.out.println(getTagValue("ATT_FILE_NO_MK", eElement)); //썸네일
                         int manual = 1;
                         while (true) {
-                            System.out.println("만드는 법  : " + getTagValue("MANUAL0" + manual, eElement));
-                            System.out.println("만드는 이미지  : " + getTagValue("MANUAL_IMG0" + manual, eElement));
+                            if (getTagValue("MANUAL0" + manual, eElement) == null) {
+                                manual += 1;
+                                continue;
+                            } else {
+                                System.out.println(getTagValue("MANUAL0" + manual, eElement).toString()); //만드는 법
+                                System.out.println(getTagValue("MANUAL_IMG0" + manual, eElement)); //만드는 이미지
+                                manual += 1;
+                            }
 
-                            manual += 1;
+
                             if (getTagValue("MANUAL0" + manual, eElement) == null && getTagValue("MANUAL0" + (manual + 1), eElement) == null && getTagValue("MANUAL0" + (manual + 2), eElement) == null) {
                                 break;
                             }
@@ -63,9 +81,9 @@ public class XmlParsing_test {
                     }    // for end
                 }    // if end
 
-                startIdx = endIdx;
-                endIdx += 1;
-                System.out.println("page number : " + page);
+                startIdx += 2;
+                endIdx += 2;
+//                System.out.println("page number : " + page);
                 page += 1;
                 if (page > 1114) {
                     break;
