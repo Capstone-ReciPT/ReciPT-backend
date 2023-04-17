64174697a67c672f297275fb899feee0b4cef82c
JHyun0302 <wogustkfkd11@gmail.com>
2023-04-17 13:43:51 +0900

    로그인 기능 구현

--- ReciPT/src/main/java/samdasu/recipt/service/UserService.java
+++ ReciPT/src/main/java/samdasu/recipt/service/UserService.java
@@ -3,8 +3,11 @@ package samdasu.recipt.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import samdasu.recipt.controller.dto.UserResponseDto;
+import samdasu.recipt.controller.dto.UserSignUpDto;
+import samdasu.recipt.entity.User;
+import samdasu.recipt.exception.ResourceNotFoundException;
 import samdasu.recipt.repository.UserRepository;
-import samdasu.recipt.service.dto.UserResponseDto;
 
 @Service
 @Transactional(readOnly = true)
@@ -12,7 +15,24 @@ import samdasu.recipt.service.dto.UserResponseDto;
 public class UserService {
     private final UserRepository userRepository;
 
-    public UserResponseDto findById(Long userId) {
+    @Transactional
+    public Long join(UserSignUpDto signUpDto) {
+        userRepository.findByLoginId(signUpDto.getLoginId())
+                .ifPresent(user -> {
+                    throw new IllegalArgumentException("Fail: Already Exist ID!");
+                });
+        if (!signUpDto.getPassword().equals(signUpDto.getPasswordConfirm())) {
+            throw new IllegalArgumentException("Fail: Please Check Password!");
+        }
 
+        User user = new User(signUpDto.getUserName(), signUpDto.getLoginId(), signUpDto.getPassword(), userRepository.findByUserAllergy(signUpDto.getUserAllergy()));
+        return userRepository.save(user).getUserId();
     }
+
+    public UserResponseDto findById(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("Fail:No UserResponseDto Info"));
+        return new UserResponseDto(user);
+    }
+
 }
