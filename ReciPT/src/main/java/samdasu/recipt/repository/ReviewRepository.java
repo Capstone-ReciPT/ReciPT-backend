62e6e73aeba958b1ac0864339df43d233d3d201f
JHyun0302 <wogustkfkd11@gmail.com>
2023-04-22 23:09:36 +0900

    repository

--- ReciPT/src/main/java/samdasu/recipt/repository/ReviewRepository.java
+++ ReciPT/src/main/java/samdasu/recipt/repository/ReviewRepository.java
@@ -4,16 +4,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import samdasu.recipt.entity.Review;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
-    List<Review> findTop10ViewCountBy();
+    List<Review> findTop10ViewCountBy(); //조회 수 탑10
 
-    Review findOne(Long reviewId);
-    //제목 조회
-    //글쓴이 조회
+    Optional<Review> findById(Long reviewId);
+
+    Review findByTitle(String title); //제목 조회
 
 
-    //조회수 조회
+//    Optional<Review> findByUserName(String userName);//글쓴이 조회
 
 }
