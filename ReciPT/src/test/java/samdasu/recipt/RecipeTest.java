835e63f52123cc758aed18d391d784a83c6e80e9
JHyun0302 <wogustkfkd11@gmail.com>
2023-04-23 23:07:18 +0900

    레시피 test

--- ReciPT/src/test/java/samdasu/recipt/RecipeTest.java
+++ ReciPT/src/test/java/samdasu/recipt/RecipeTest.java
@@ -8,5 +8,11 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class RecipeTest {
-    
+    /**
+     * DB + Gpt 레시피 좋아요 탑 10
+     */
+
+    /**
+     * DB + Gpt 레시피 조회 수 탑 10
+     */
 }
