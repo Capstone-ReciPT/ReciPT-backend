b619f63dd93821daf29ad04d8280075a2b9e1ccd
JHyun0302 <wogustkfkd11@gmail.com>
2023-04-23 23:04:34 +0900

    review dto

--- ReciPT/src/main/java/samdasu/recipt/controller/dto/Review/ReviewRequestDto.java
+++ ReciPT/src/main/java/samdasu/recipt/controller/dto/Review/ReviewRequestDto.java
@@ -2,24 +2,34 @@ package samdasu.recipt.controller.dto.Review;
 
 import lombok.Getter;
 import lombok.Setter;
-import samdasu.recipt.controller.dto.ImageFileDto;
+import samdasu.recipt.controller.dto.ImageFile.ImageFileDto;
 import samdasu.recipt.entity.DbRecipe;
 import samdasu.recipt.entity.GptRecipe;
 import samdasu.recipt.entity.Review;
 import samdasu.recipt.entity.User;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Getter
 @Setter
 public class ReviewRequestDto {
+    @NotBlank
     private User user;
+    @NotNull
     private Long reviewId;
+    @NotNull
     private String userName;
+    @NotNull
     private String title;
+    @NotNull
     private String comment;
+    @NotNull
     private Integer viewCount;
+    @NotNull
+    private Integer likeCount;
     private GptRecipe gptRecipe;
     private DbRecipe dbRecipe;
     private List<ImageFileDto> imageFiles;
@@ -31,6 +41,7 @@ public class ReviewRequestDto {
         title = review.getTitle();
         comment = review.getComment();
         viewCount = review.getViewCount();
+        likeCount = review.getLikeCount();
         gptRecipe = review.getGptRecipe();
         dbRecipe = review.getDbRecipe();
         imageFiles = review.getImageFiles().stream()
