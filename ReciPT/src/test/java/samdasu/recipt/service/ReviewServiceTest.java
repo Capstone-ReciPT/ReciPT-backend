788a536bfa4e02655ee4ed2a26c0ab9c8654ec0c
JHyun0302 <wogustkfkd11@gmail.com>
2023-04-26 13:15:22 +0900

    관심사 분리

--- ReciPT/src/test/java/samdasu/recipt/service/ReviewServiceTest.java
+++ ReciPT/src/test/java/samdasu/recipt/service/ReviewServiceTest.java
@@ -4,111 +4,211 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
-import samdasu.recipt.controller.dto.ImageFile.ImageFileRequestDto;
-import samdasu.recipt.controller.dto.ImageFile.ImageFileResponseDto;
 import samdasu.recipt.controller.dto.Review.ReviewRequestDto;
 import samdasu.recipt.entity.Allergy;
 import samdasu.recipt.entity.GptRecipe;
 import samdasu.recipt.entity.Review;
 import samdasu.recipt.entity.User;
+import samdasu.recipt.exception.ResourceNotFoundException;
 import samdasu.recipt.repository.GptRecipe.GptRecipeRepository;
 import samdasu.recipt.repository.Review.ReviewRepository;
 import samdasu.recipt.repository.UserRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @Transactional
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ReviewServiceTest {
+    @PersistenceContext
+    EntityManager em;
+
+    @Autowired
+    ImageFileService imageFileService;
+    @Autowired
+    UserRepository userRepository;
     @Autowired
     UserService userService;
     @Autowired
     GptRecipeRepository gptRecipeRepository;
     @Autowired
+    GptRecipeService gptRecipeService;
+
+    @Autowired
     ReviewRepository reviewRepository;
     @Autowired
     ReviewService reviewService;
-    @Autowired
-    UserRepository userRepository;
+
 
     @Test
-    @Rollback(value = false)
     public void 조회수_증가() throws Exception {
         //given
-        User user = User.createUser("tester1", "testId", "test1234", "shrimp");
-        Allergy allergy = Allergy.createAllergy("갑각류", "새우");
-        GptRecipe gptRecipe = GptRecipe.createGptRecipe("계란말이", "계란, 파, 당근", "1.손질 2.굽기 3.먹기", "케찹 추가 맛있어요.", 0, 0, 0.0, 0, allergy);
-
-        Review gptReview = Review.createGptReview("계란말이 후기", "단짠단짠", 0, 0, user, gptRecipe);
-
-        userRepository.save(user);
-        gptRecipeRepository.save(gptRecipe);
-        reviewRepository.save(gptReview);
+        Review review = createReview();
 
         //when
-        reviewService.increaseViewCount(gptReview.getReviewId());
+        reviewService.increaseViewCount(review.getReviewId());
 
         //then
-        assertEquals(gptReview.getViewCount(), 1);
+        assertEquals(review.getViewCount(), 1);
     }
 
     @Test
     public void 리뷰_좋아요_증가() throws Exception {
         //given
-        User user = User.createUser("tester1", "testId", "test1234", "shrimp");
-        Allergy allergy = Allergy.createAllergy("갑각류", "새우");
-        GptRecipe gptRecipe = GptRecipe.createGptRecipe("계란말이", "계란, 파, 당근", "1.손질 2.굽기 3.먹기", "케찹 추가 맛있어요.", 0, 0, 0.0, 0, allergy);
-
-        Review gptReview = Review.createGptReview("계란말이 후기", "단짠단짠", 0, 0, user, gptRecipe);
-
-        userRepository.save(user);
-        gptRecipeRepository.save(gptRecipe);
-        reviewRepository.save(gptReview);
+        Review review = createReview();
 
         //when
-        reviewService.increaseReviewLike(gptReview.getReviewId());
+        reviewService.increaseReviewLike(review.getReviewId());
         //then
-        assertEquals(gptReview.getLikeCount(), 1);
+        assertEquals(review.getLikeCount(), 1);
     }
 
     @Test
     public void 리뷰_좋아요_감소() throws Exception {
-        User user = User.createUser("tester1", "testId", "test1234", "shrimp");
-        Allergy allergy = Allergy.createAllergy("갑각류", "새우");
-        GptRecipe gptRecipe = GptRecipe.createGptRecipe("계란말이", "계란, 파, 당근", "1.손질 2.굽기 3.먹기", "케찹 추가 맛있어요.", 0, 0, 0.0, 0, allergy);
-
-        Review gptReview = Review.createGptReview("계란말이 후기", "단짠단짠", 0, 1, user, gptRecipe);
-
-        userRepository.save(user);
-        gptRecipeRepository.save(gptRecipe);
-        reviewRepository.save(gptReview);
+        //given
+        Review review = createReview();
 
         //when
-        reviewService.decreaseReviewLike(gptReview.getReviewId());
+        reviewService.decreaseReviewLike(review.getReviewId());
         //then
-        assertEquals(gptReview.getLikeCount(), 0);
+        assertEquals(review.getLikeCount(), -1);
     }
 
     @Test
-    @Rollback(value = false)
     public void 리뷰_저장() throws Exception {
         //given
-        Allergy allergy = Allergy.createAllergy("갑각류", "새우");
-        GptRecipe gptRecipe = GptRecipe.createGptRecipe("계란말이", "계란, 파, 당근", "1.손질 2.굽기 3.먹기", "케찹 추가 맛있어요.", 0, 0, 0.0, 0, allergy);
+        GptRecipe recipe = createRecipe();
+        ReviewRequestDto gptReviewRequestDto = ReviewRequestDto.createGptReviewRequestDto("testerA", "리뷰 제목1", "계란말이 맛있다.", recipe, null);
 
-        ImageFileRequestDto imageFileRequestDto = ImageFileRequestDto.createImageFileRequestDto("qwe-qwe-qwe", "egg-food");
-
-
-        ImageFileResponseDto.createImageFileResponseDto()
-        ReviewRequestDto gptReviewRequestDto = ReviewRequestDto.createGptReviewRequestDto("tester", "리뷰 제목1", "계란말이 맛있다.", gptRecipe, );
-        reviewService.saveReview(gptReviewRequestDto);
         //when
+        reviewService.saveReview(gptReviewRequestDto);
 
         //then
+        assertThat(gptReviewRequestDto.getTitle()).isEqualTo("리뷰 제목1");
+    }
+
+    @Test
+    public void 리뷰_업데이트() throws Exception {
+        //given
+        GptRecipe gptRecipe = createRecipe();
+        Review review = createReview();
+
+        ReviewRequestDto gptReviewRequestDto = ReviewRequestDto.createGptReviewRequestDto("tester2", "changeTitle", "changeComment", gptRecipe, null);
+        //when
+        Long updateReview = reviewService.update(review.getReviewId(), gptReviewRequestDto);
+
+        Review findReview = reviewRepository.findById(updateReview).get();
+        //then
+        assertEquals(findReview.getTitle(), "changeTitle");
     }
 
 
+    @Test
+    public void 리뷰_삭제() throws Exception {
+        //given
+        Review review = createReview();
+
+        //when
+        reviewService.delete(review.getReviewId());
+
+        //then
+        long count = reviewRepository.count();
+        assertThat(count).isEqualTo(18);
+
+    }
+
+    @Test
+    public void 리뷰_조회수_탑10() throws Exception {
+        //given
+
+        //when
+        System.out.println("############### 조회수 top10 ###############");
+        List<Review> top10ViewCount = reviewService.findTop10ViewCount();
+        //then
+
+        for (Review review : top10ViewCount) {
+            System.out.println("review.getViewCount() = " + review.getViewCount());
+        }
+    }
+
+    @Test
+    public void Review_좋아요_탑_10() throws Exception {
+        //given
+
+        //when
+        System.out.println("############### 좋아요 수 top10 ###############");
+        List<Review> top10LikeCount = reviewService.findTop10LikeCount();
+        //then
+
+        for (Review review : top10LikeCount) {
+            System.out.println("review.getLikeCount() = " + review.getLikeCount());
+        }
+    }
+
+
+    @Test
+    public void Review_제목_조회() throws Exception {
+        //given
+        Review findReview = createReview();
+
+        //when
+        Review review = reviewService.findReviewByTitle(findReview.getTitle())
+                .orElseThrow(() -> new ResourceNotFoundException("Fail: No Review Info"));
+
+        //then
+        assertEquals(review.getTitle(), "파스타 후기");
+    }
+
+    @Test
+    public void Review_글쓴이_조회() throws Exception {
+        //given
+
+        //when
+        List<Review> findReviewsByWriter = reviewService.findReviewByWriter("testerA"); // 'testerA' 찾기
+
+        //then
+        assertEquals(findReviewsByWriter.size(), 2);
+        for (Review review : findReviewsByWriter) {
+            System.out.println("review.getUser().getUserName() = " + review.getUser().getUsername());
+        }
+    }
+
+    @Test
+    public void Review_전체조회() throws Exception {
+        //given
+
+        //when
+        List<Review> reviews = reviewService.findAll();
+
+        //then
+        assertThat(reviews.size()).isEqualTo(18);
+    }
+
+    private Review createReview() {
+        User user = User.createUser("testerE", "testE", "E1234", "bean");
+        em.persist(user);
+
+        Allergy allergy = Allergy.createAllergy("갑각류", "새우");
+
+        GptRecipe gptRecipe = GptRecipe.createGptRecipe("파스타", "면, 소스", "1.삶기 2.먹기", "소스 맛은 기호에 따라 바꾸세요!", 0, 0, 0.0, 0, allergy);
+        em.persist(gptRecipe);
+
+        Review review = Review.createGptReview("파스타 후기", "면이 쫀득쫀득", 0, 0, user, gptRecipe);
+        em.persist(review);
+        return review;
+    }
+
+    private GptRecipe createRecipe() {
+        Allergy allergy = Allergy.createAllergy("갑각류", "새우");
+        GptRecipe gptRecipe = GptRecipe.createGptRecipe("만두", "고기피, 만두피", "1.만두 빚기 2.굽기 3.먹기", "음료수랑 먹으면 맛있어요.", 0, 0, 0.0, 0, allergy);
+        em.persist(gptRecipe);
+
+        return gptRecipe;
+    }
 }
--- ReciPT/src/test/java/samdasu/recipt/service/UserServiceTest.java
+++ ReciPT/src/test/java/samdasu/recipt/service/UserServiceTest.java
@@ -10,7 +10,11 @@ import samdasu.recipt.controller.dto.User.UserResponseDto;
 import samdasu.recipt.controller.dto.User.UserSignUpDto;
 import samdasu.recipt.controller.dto.User.UserUpdateRequestDto;
 import samdasu.recipt.entity.User;
+import samdasu.recipt.exception.ResourceNotFoundException;
 import samdasu.recipt.repository.UserRepository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -19,7 +23,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @Transactional
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserServiceTest {
-
+    @PersistenceContext
+    EntityManager em;
     @Autowired
     UserRepository userRepository;
 
@@ -29,20 +34,20 @@ public class UserServiceTest {
     @Autowired
     BCryptPasswordEncoder passwordEncoder;
 
+
     @Test
     public void 유저_회원가입() {
         //given
         UserSignUpDto signUpDto = new UserSignUpDto("tester", "testId", "test1234", "test1234", "shrimp");
-//        UserSignUpDto kim = new UserSignUpDto("kim", "id2", "5678", "5678", "milk");
 
         //when
         Long savedId = userService.join(signUpDto);
-//        Long savedKim = userService.join(kim);
 
         //then
-        User user = userRepository.findById(savedId).get();
+        User user = userRepository.findById(savedId)
+                .orElseThrow(() -> new ResourceNotFoundException("Fail: No User Info"));
 
-        assertThat(user.getUserName()).isEqualTo("tester");
+        assertThat(user.getUsername()).isEqualTo("tester");
         assertThat(user.getLoginId()).isEqualTo("testId");
         assertThat(passwordEncoder.matches("test1234", user.getPassword())).isEqualTo(true);
     }
@@ -52,8 +57,7 @@ public class UserServiceTest {
         // given
         UserSignUpDto tester1 = new UserSignUpDto("tester1", "testId", "test1234", "test1234", "shrimp");
 
-
-        Long savedId = userService.join(tester1);
+        userService.join(tester1);
 
         // when
         // same loginId
@@ -70,36 +74,45 @@ public class UserServiceTest {
     @Test
     public void 유저_회원정보_조회() {
         //given
-        User user = User.createUser("tester1", "testId", "test1234", "shrimp");
-        User savedUser = userRepository.save(user);
-        UserResponseDto userResponseDto = new UserResponseDto(savedUser);
+        User testA = createUser();
+        UserResponseDto userResponseDto = new UserResponseDto(testA);
 
         //when
-        UserResponseDto findUser = userService.findById(userResponseDto.getUserId());
+        UserResponseDto findUser = findUserResponseDtoById(userResponseDto.getUserId());
 
         //then
-        assertThat(findUser.getUserId()).isEqualTo(user.getUserId());
-        assertThat(findUser.getUserName()).isEqualTo(user.getUserName());
-        assertThat(findUser.getLoginId()).isEqualTo(user.getLoginId());
+        assertThat(findUser.getUserId()).isEqualTo(testA.getUserId());
+        assertThat(findUser.getUsername()).isEqualTo(testA.getUsername());
+        assertThat(findUser.getLoginId()).isEqualTo(testA.getLoginId());
     }
 
     @Test
     public void 유저_업데이트() {
         //given
-        User user = User.createUser("tester1", "testId", "test1234", "shrimp");
-        User savedUser = userRepository.save(user);
-        UserResponseDto userResponseDto = new UserResponseDto(savedUser);
+        User testA = createUser();
+        UserResponseDto userResponseDto = new UserResponseDto(testA);
 
         //when
         //change password & Allergy Info
         UserUpdateRequestDto userUpdateRequestDto = UserUpdateRequestDto.createUpdateUserInfo("changePassword", "changeAllergy");
         Long updateUserInfo = userService.update(userResponseDto.getUserId(), userUpdateRequestDto);
 
-
         //then
-        assertThat(updateUserInfo).isEqualTo(userResponseDto.getUserId());
-        UserResponseDto updateUser = userService.findById(updateUserInfo);
+        UserResponseDto updateUser = findUserResponseDtoById(updateUserInfo);
+
         assertThat(updateUser.getPassword()).isEqualTo(userUpdateRequestDto.getPassword());
         assertThat(updateUser.getUserAllergy()).isEqualTo(userUpdateRequestDto.getUserAllergy());
+    }
+
+    public UserResponseDto findUserResponseDtoById(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("Fail: No User Info"));
+        return new UserResponseDto(user);
+    }
+
+    private User createUser() {
+        User user = User.createUser("testerC", "testC", "C1234", "bean");
+        em.persist(user);
+        return user;
     }
 }
