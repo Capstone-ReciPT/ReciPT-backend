41567e044ca28c044362bf650e604870654c46c4
zzun <736300@naver.com>
2023-05-21 19:32:39 +0900

    gpt - 버튼형식으로 변경 / 응답값 json 추출 및 예외처리 / 챗 테스트 웹 개발

--- ReciPT/src/main/java/samdasu/recipt/api/gpt/controller/ChatGptApiController.java
+++ ReciPT/src/main/java/samdasu/recipt/api/gpt/controller/ChatGptApiController.java
@@ -17,7 +17,6 @@ import samdasu.recipt.domain.controller.dto.User.UserResponseDto;
 import samdasu.recipt.domain.service.GptService;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -60,28 +59,133 @@ public class ChatGptApiController {
             String responseMessage = chatgptService.getResponse(conversation);
             log.info("requestId {}, ip {}\n get a reply:\n {}", requestId, request.getRemoteHost(), responseMessage);
 
-            if (isSaveChat(content)) {
-                Pattern pattern = Pattern.compile("\\{.*\\}");
-                Matcher matcher = pattern.matcher(responseMessage);
-                if (matcher.find()) {
-                    String jsonString = matcher.group();
-
-                    try {
-                        Long gptRecipeId = saveGptPrompt(jsonString, userResponseDto.getUserId());
-                        clearConversation();
-                        log.info("Saved GptRecipe. foodName: {}", gptService.getGptRecipeByGptId(gptRecipeId).getFoodName());
-                    } catch (IOException e) {
-                        log.error("Failed to extract values from JSON response", e);
-                        return ResponseModel.fail("Failed to extract values from JSON response");
-                    }
-                }
-            }
-
             return ResponseModel.success(responseMessage);
         } catch (Exception e) {
             log.error("Error occurred during sendContent", e);
             return ResponseModel.fail("Error occurred during the request.");
         }
+    }
+
+    @PostMapping("/search")
+    public ResponseModel<String> searchFoodRecipe(@AuthenticationPrincipal UserResponseDto userResponseDto, HttpServletRequest request, @RequestBody String content) {
+        try {
+            if (StringUtils.isEmpty(content)) {
+                return ResponseModel.fail("Content is required.");
+            }
+            String requestId = UUID.randomUUID().toString();
+            log.info("requestId {}, ip {}, send content: {}", requestId, request.getRemoteHost(), content);
+
+            Message userMessage = new Message(USER, content);
+
+            if (CollectionUtils.isEmpty(conversation)) {
+                Message systemMessage = new Message(SYSTEM, SEARCHBAR_SYSTEM_TASK_MESSAGE);
+                conversation.add(systemMessage);
+            }
+            conversation.add(userMessage);
+
+            String responseMessage = chatgptService.getResponse(conversation);
+            log.info("requestId {}, ip {}\n get a reply:\n {}", requestId, request.getRemoteHost(), responseMessage);
+
+            return ResponseModel.success(responseMessage);
+        } catch (Exception e) {
+            log.error("Error occurred during searchFoodRecipe", e);
+            return ResponseModel.fail("Error occurred during the request.");
+        }
+    }
+
+    @PostMapping("/refresh")
+    public ResponseModel<String> refreshConversation() {
+        clearConversation();
+        return ResponseModel.success("Conversation refreshed successfully.");
+    }
+
+    @PostMapping("/edit")
+    public ResponseModel<String> editGptRecipe(@AuthenticationPrincipal UserResponseDto userResponseDto, @RequestBody String content) {
+        try {
+            if (StringUtils.isEmpty(content)) {
+                return ResponseModel.fail("Content is required.");
+            }
+            Message userMessage = new Message(USER, "[수정] " + content);
+            conversation.add(userMessage);
+            String responseMessage = chatgptService.getResponse(conversation);
+            return ResponseModel.success(responseMessage);
+        } catch (Exception e) {
+            log.error("Error occurred during editGptRecipe", e);
+            return ResponseModel.fail("Error occurred during the request.");
+        }
+    }
+
+    @PostMapping("/save")
+    public ResponseModel<String> saveGptRecipe(@AuthenticationPrincipal UserResponseDto userResponseDto) {
+        try {
+            Message userMessage = new Message(USER, SAVE_APPEND_MESSAGE + "[저장]");
+            conversation.add(userMessage);
+            String responseMessage = chatgptService.getResponse(conversation);
+
+            int startIndex = responseMessage.indexOf("{");
+            int endIndex = responseMessage.lastIndexOf("}");
+
+            if (startIndex >= 0) {
+                if (endIndex == -1 || endIndex < startIndex) {
+                    endIndex = responseMessage.length();
+                }
+
+                String jsonResponse = responseMessage.substring(startIndex, endIndex + 1);
+
+                Long gptRecipeId = saveGptPrompt_V2(jsonResponse, userResponseDto);
+//                Long gptRecipeId = gptService.createGptRecipe(foodName, ingredient, context, 2L);
+                clearConversation();
+                log.info("Saved GptRecipe. foodName: {}", gptService.getGptRecipeByGptId(gptRecipeId).getFoodName());
+            }
+//            Pattern pattern1 = Pattern.compile("\"\\{.*\\}\"");
+//            Pattern pattern2 = Pattern.compile("\\{.*\\}");
+//            Matcher matcher1 = pattern1.matcher(responseMessage);
+//            Matcher matcher2 = pattern2.matcher(responseMessage);
+//            if (matcher1.find()) {
+//                String jsonString = matcher1.group();
+//
+//                try {
+//                    Long gptRecipeId = saveGptPrompt(jsonString, userResponseDto.getUserId());
+//                    clearConversation();
+//                    log.info("Saved GptRecipe. foodName: {}", gptService.getGptRecipeByGptId(gptRecipeId).getFoodName());
+//                } catch (IOException e) {
+//                    log.error("Failed to extract values from JSON response", e);
+//                    return ResponseModel.fail("Failed to extract values from JSON response");
+//                }
+//            } else if (matcher2.find()) {
+//                String jsonString = matcher2.group();
+//
+//                try {
+//                    Long gptRecipeId = saveGptPrompt(jsonString, userResponseDto.getUserId());
+//                    clearConversation();
+//                    log.info("Saved GptRecipe. foodName: {}", gptService.getGptRecipeByGptId(gptRecipeId).getFoodName());
+//                } catch (IOException e) {
+//                    log.error("Failed to extract values from JSON response", e);
+//                    return ResponseModel.fail("Failed to extract values from JSON response");
+//                }
+//
+//            }
+            return ResponseModel.success(responseMessage);
+        } catch (Exception e) {
+            log.error("Error occurred during saveGptRecipe", e);
+            return ResponseModel.fail("Error occurred during the request.");
+        }
+    }
+
+    private Long saveGptPrompt_V2(String jsonResponse, UserResponseDto userResponseDto) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode responseJson = objectMapper.readTree(jsonResponse);
+        String foodName = responseJson.path("foodName").asText();
+        String ingredient = responseJson.path("ingredient").asText();
+        String context = responseJson.path("context").asText();
+
+        if (StringUtils.isEmpty(foodName) || StringUtils.isEmpty(ingredient) || StringUtils.isEmpty(context)) {
+            log.error("Failed to extract values from JSON response. Missing required fields.");
+            throw new IllegalArgumentException("Missing required fields in JSON response");
+        }
+
+        Long gptRecipeId = gptService.createGptRecipe(foodName, ingredient, context, userResponseDto.getUserId());
+        return gptRecipeId;
     }
 
     private Long saveGptPrompt(String jsonString, Long userId) throws JsonProcessingException {
@@ -94,29 +198,8 @@ public class ChatGptApiController {
         return gptRecipeId;
     }
 
-    @PostMapping("/refresh")
-    public ResponseModel<String> refreshConversation() {
-        clearConversation();
-//        conversation.removeIf(message -> !"system".equals(message.getRole()));
-        return ResponseModel.success("Conversation refreshed successfully.");
-    }
-
-    private boolean isSaveChat(String content) {
-        return content.contains(SAVE_MESSAGE);
-    }
-
     private void clearConversation() {
         conversation.clear();
     }
 
-    //    private boolean isEndChat(String responseMessage) {
-//        try {
-//            ObjectMapper objectMapper = new ObjectMapper();
-//            JsonNode responseJson = objectMapper.readTree(responseMessage);
-//            return responseJson.has("foodName") && responseJson.has("ingredient") && responseJson.has("context");
-//        } catch (IOException e) {
-//            log.error("Failed to parse response message as JSON", e);
-//            return false;
-//        }
-//    }
 }
