9ef42d4176d8c725b9dfacca38ac9e25ffc48b84
JHyun0302 <wogustkfkd11@gmail.com>
2023-05-21 20:53:46 +0900

    프롬프트 변경

--- ReciPT/src/main/java/samdasu/recipt/api/gpt/controller/ChatGptApiController.java
+++ ReciPT/src/main/java/samdasu/recipt/api/gpt/controller/ChatGptApiController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 import samdasu.recipt.api.gpt.controller.dto.ResponseModel;
 import samdasu.recipt.api.gpt.dto.chat.Message;
 import samdasu.recipt.api.gpt.service.ChatGptService;
-import samdasu.recipt.domain.controller.dto.Gpt.GptResponseDto;
 import samdasu.recipt.domain.controller.dto.User.UserResponseDto;
 import samdasu.recipt.domain.service.GptService;
 
@@ -42,8 +41,42 @@ public class ChatGptApiController {
 
     private List<Message> conversation = new ArrayList<>();
 
-    @PostMapping("/send")
-    public ResponseModel<String> sendContent(@AuthenticationPrincipal UserResponseDto userResponseDto, HttpServletRequest request, @RequestBody String content) {
+    /**
+     * 검색결과 X -> 레시피
+     */
+    @GetMapping("/search")
+    public ResponseModel<String> getSearchInfo(@AuthenticationPrincipal UserResponseDto userResponseDto, HttpServletRequest request, @RequestBody String content) {
+        try {
+            if (StringUtils.isEmpty(content)) {
+                return ResponseModel.fail("Content is required.");
+            }
+            String requestId = UUID.randomUUID().toString();
+            log.info("requestId {}, ip {}, send content: {}", requestId, request.getRemoteHost(), content);
+
+            Message userMessage = new Message(USER, content);
+
+            if (CollectionUtils.isEmpty(conversation)) {
+                Message systemMessage = new Message(SYSTEM, NoSearching_SYSTEM_MESSAGE);
+                conversation.add(systemMessage);
+            }
+
+            conversation.add(userMessage);
+
+            String responseMessage = chatgptService.getResponse(conversation);
+            log.info("requestId {}, ip {}\n get a reply:\n {}", requestId, request.getRemoteHost(), responseMessage);
+
+            return ResponseModel.success(responseMessage);
+        } catch (Exception e) {
+            log.error("Error occurred during sendContent", e);
+            return ResponseModel.fail("Error occurred during the request.");
+        }
+    }
+
+    /**
+     * "욜로 + stt" (식재료) -> 음식 선택 -> 레시피
+     */
+    @GetMapping("/response")
+    public ResponseModel<String> gptResponse(@AuthenticationPrincipal UserResponseDto userResponseDto, HttpServletRequest request, @RequestBody String content) {
         try {
             if (StringUtils.isEmpty(content)) {
                 return ResponseModel.fail("Content is required.");
@@ -63,25 +96,6 @@ public class ChatGptApiController {
             String responseMessage = chatgptService.getResponse(conversation);
             log.info("requestId {}, ip {}\n get a reply:\n {}", requestId, request.getRemoteHost(), responseMessage);
 
-//            if (isSaveChat(content)) {
-//                Pattern pattern = Pattern.compile("\\{.*\\}");
-//                Matcher matcher = pattern.matcher(responseMessage);
-//                if (matcher.find()) {
-//                    String jsonString = matcher.group();
-//
-//                    try {
-//                        Long gptRecipeId = saveGptPrompt(jsonString, userResponseDto.getUserId());
-//                        clearConversation();
-//                        log.info("Saved GptRecipe. foodName: {}", gptService.getGptRecipeByGptId(gptRecipeId).getFoodName());
-//                    } catch (IOException e) {
-//                        log.error("Failed to extract values from JSON response", e);
-//                        return ResponseModel.fail("Failed to extract values from JSON response");
-//                    }
-//                }
-//            }
-
-            gptResponse(userResponseDto, request, responseMessage);
-
             return ResponseModel.success(responseMessage);
         } catch (Exception e) {
             log.error("Error occurred during sendContent", e);
@@ -90,7 +104,26 @@ public class ChatGptApiController {
     }
 
     /**
-     * 저장 버튼 클릭
+     * 수정 버튼
+     */
+    @GetMapping("/edit")
+    public ResponseModel<String> editResponse(@AuthenticationPrincipal UserResponseDto userResponseDto, HttpServletRequest request, @RequestBody String content) {
+        conversation.add(new Message(USER, "[수정]"));
+        return gptResponse(userResponseDto, request, content);
+    }
+
+    /**
+     * 수정 버튼
+     */
+    @GetMapping("/editsearch")
+    public ResponseModel<String> editSearchInfo(@AuthenticationPrincipal UserResponseDto userResponseDto, HttpServletRequest request, @RequestBody String content) {
+        conversation.add(new Message(USER, "[수정]"));
+        return getSearchInfo(userResponseDto, request, content);
+    }
+
+
+    /**
+     * 저장 버튼
      */
     @PostMapping("/save")
     public ResponseModel<String> saveResponse(@AuthenticationPrincipal UserResponseDto userResponseDto) {
@@ -111,18 +144,6 @@ public class ChatGptApiController {
             }
         }
         return ResponseModel.success(responseMessage);
-    }
-
-    /**
-     * gpt 응답 내용 플러터에 보이기
-     */
-    @GetMapping
-    public Result gptResponse(@AuthenticationPrincipal UserResponseDto userResponseDto, HttpServletRequest request, String responseMessage) {
-        String requestId = UUID.randomUUID().toString();
-
-        log.info("requestId {}, ip {}\n in flutter:\n {}", requestId, request.getRemoteHost(), responseMessage);
-        GptResponseDto gptResponseDto = GptResponseDto.createGptResponseDto(responseMessage);
-        return new Result(gptResponseDto);
     }
 
 
