f37bc59f64e9aebe48aa2bd51d20c70e764da4e9
JHyun0302 <wogustkfkd11@gmail.com>
2023-09-13 11:39:59 +0900

    :truck: [Refactor] 패키지 구조 변경

--- ReciPT/src/main/java/samdasu/recipt/domain/controller/constant/ControllerStandard.java
+++
@@ -1,5 +0,0 @@
-package samdasu.recipt.domain.controller.constant;
-
-public class ControllerStandard {
-    public static final Integer STANDARD = 10;
-}
--- ReciPT/src/main/java/samdasu/recipt/domain/repository/Review/ReviewRepository.java
+++
@@ -1,8 +0,0 @@
-package samdasu.recipt.domain.repository.Review;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import samdasu.recipt.domain.entity.Review;
-
-public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewCustomRepository {
-    
-}
