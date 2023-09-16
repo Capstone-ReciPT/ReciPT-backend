0fab808b46ba1dde0c744bd658972c72d623e295
JHyun0302 <wogustkfkd11@gmail.com>
2023-09-16 10:59:09 +0900

    :art: [Feat] @Import 이용해 Aspect 적용

--- src/main/java/samdasu/recipt/ReciPtApplication.java
+++ src/main/java/samdasu/recipt/ReciPtApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import samdasu.recipt.domain.aop.*;
 
 @EnableScheduling
 @SpringBootApplication
+@Import({UserAspect.class, ReviewAspect.class, RegisterRecipeAspect.class, RecipeAspect.class, HeartAspect.class, GptAspect.class})
 public class ReciPtApplication {
 
     @Bean
